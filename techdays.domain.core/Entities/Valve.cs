907bf6b9e5a794d193b9dc58283afbb8f93800c5
digitalpedro <!Mickey1010>
2015-02-06 06:38:59 +0100

    Added entity "Valve"

---
+++ techdays.domain.core/Entities/Valve.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace techdays.domain.core.Entities
+{
+    public class Valve
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Sequence { get; set; }
+    }
+}
