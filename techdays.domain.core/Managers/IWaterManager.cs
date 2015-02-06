022d681bb6fa746afd9e2bbe7f2f621432453dcd
digitalpedro <!Mickey1010>
2015-02-06 06:44:07 +0100

    Defined interface "IWaterManager" with a watering method

---
+++ techdays.domain.core/Managers/IWaterManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace techdays.domain.core.Managers
+{
+    public interface IWaterManager
+    {
+        /// <summary>
+        /// Provide the designated amount of water to a specified plant
+        /// </summary>
+        /// <param name="deviceId">Id of the plant that is to be watered</param>
+        /// <param name="waterAmountInMiliLiters">Amount of water to provide, in milliliters</param>
+        void GiveWater(Guid plantId, int waterAmountInMiliLiters);
+    }
+}
